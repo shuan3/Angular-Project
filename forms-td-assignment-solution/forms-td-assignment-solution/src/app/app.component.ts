c16190d64b4ae807ad4eea5b52d038989de27c0a
Raymond Huang <shanhua18@gmail.com>
2024-04-16 07:19:25 -0400

    update practice

---
+++ forms-td-assignment-solution/forms-td-assignment-solution/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  @ViewChild('signupForm', { static: false }) sgnForm: NgForm;
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
+}
