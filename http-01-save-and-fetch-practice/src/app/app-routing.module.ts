739c10a2456fa51ea3e24de9b502b04561ff58e8
Raymond Huang <shanhua18@gmail.com>
2024-05-02 07:20:48 -0400

    run universal

--- http-01-save-and-fetch-practice/src/app/app-routing.module.ts
+++ http-01-save-and-fetch-practice/src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { ReceipesResolverService } from './recipes/receipts-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent , resolve:[ReceipesResolverService]},
+    { path: ':id/edit', component: RecipeEditComponent, resolve:[ReceipesResolverService]},
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
--- http-01-save-and-fetch-practice/src/app/shared/data-storage.service.ts
+++ http-01-save-and-fetch-practice/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -22,7 +22,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
       )
@@ -34,10 +34,12 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : []
             };
           });
+        }),tap(recipes=>{
+          this.recipeService.setRecipes(recipes);
         })
       )
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-      });
+      // .subscribe(recipes => {
+      //   this.recipeService.setRecipes(recipes);
+      // });
   }
 }
